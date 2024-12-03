806eceb5363f4f09fbbad8980c418f59de2a2b8e
Jakob Louis Therkelsen <therkels@msu.edu>
2024-12-03 11:22:36 -0500

    3sum problem

---
+++ arrays/3sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res_list = []
+        for i in range(len(nums)):
+            j = i+1
+            k = len(nums)-1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    res_list.append([nums[i], nums[j], nums[k]])
+                    j+= 1
+                elif nums[i] + nums[j] + nums[k] < 0:
+                    j+=1
+                else: 
+                    k -= 1
+        return res_list
+
+if __name__ == "__main__":
+  S = Solution()
+
+  nums = [-1,0,1,2,-1,-4]
+  print(S.threeSum(nums))
