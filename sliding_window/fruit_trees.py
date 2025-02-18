4e62357c98b125aa1d468ee4615982306cdf002a
Jakob Louis Therkelsen <therkels@msu.edu>
2025-02-18 09:13:30 -0500

    fruit trees problem

--- sliding_window/fruit_trees.py
+++ sliding_window/fruit_trees.py
@@ -1,14 +1,43 @@
+from typing import List
+
+
 class Solution:
+
     def totalFruit(self, fruits: List[int]) -> int:
+        """
+        Get the maximum number of fruits that can be pulled from an array of "trees". 
+        (problem:https://leetcode.com/problems/fruit-into-baskets/description/)
+
+        Args:
+          fruits (List[int]): the array of fruits
+
+        Returns:
+          int: the longest continguous length of two fruits
+        """
         max_fruit = 0
-        first_basket = 0
-        fruits = {fruits[0], fruits[1]}
-        for next_basket in range(2, len(fruits)):
-            curr_fruit = fruits(next_basket)
+        basket = {}
+        left = 0
+
+        # Sliding window, use right as the leading window (2*O(N))
+        for right in range(len(fruits)):
+            basket[fruits[right]] = basket.get(fruits[right], 0) + 1
+
+            # basket needs to be emptied (move left window until only two fruits remain)
+            while len(basket) > 2:
+                move_cnt = basket[fruits[left]]
+                basket[fruits[left]] -= 1
+                # exit condition
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+            # add one since problem is 0 indexed
+            max_fruit = max(max_fruit, right - left + 1)
+
+        return max_fruit
 
 
 if __name__ == "__main__":
-    fruits = [1, 2, 3, 2, 2]
+    fruits = [3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]
 
     S = Solution()
     print(S.totalFruit(fruits))
